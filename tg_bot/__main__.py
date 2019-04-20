1a7fd3d56da17f6b8645ba15e26ea9381cbe8e54
anandvfc <48455768+Anandsudhakaranps@users.noreply.github.com>
2019-04-20 21:30:43 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,7 +19,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! I'm a group manager bot maintained by [this wonderful person](tg://user?id={}).
+Hi {}, my name is {}! I'm a group manager bot maintained by [Anand](tg://user?id={}).
 I'm built in python3, using the python-telegram-bot library, and am not an open source..
 
 You can find the list of available commands with /help.
